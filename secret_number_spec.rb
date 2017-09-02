ecfb98ed119687b3d1b81d93416604df8649e85d
Alfredo Motta <mottalrd@users.noreply.github.com>
2017-09-02 10:19:01 +0100

    Simpler test

--- secret_number_spec.rb
+++ secret_number_spec.rb
@@ -13,27 +13,21 @@ describe(Game) do
   #let(:secret_number) { 5 }
 
   describe '#guess' do
-    for i in 1..10
-      if (i == 5)
-        context 'when guessing a number that is the SAME as the secret number ' do
-          it 'returns the symbol :found_secret_number' do
-            expect(subject.guess(i)).to eq(:found_secret_number)
-          end
-        end
-
-      elsif (i < 5)
-        context 'when guessing a number that is lower than the secret number ' do
-          it 'returns the symbol :lower' do
-            expect(subject.guess(i)).to eq(:lower)
-          end
-        end
-
-      elsif (i > 5)
-        context 'when guessing a number that is higher than the secret number ' do
-          it 'returns the symbol :greater' do
-            expect(subject.guess(i)).to eq(:greater)
-            end
-         end
+    context 'when guessing a number that is the SAME as the secret number ' do
+      it 'returns the symbol :found_secret_number' do
+        expect(subject.guess(5)).to eq(:found_secret_number)
+      end
+    end
+    
+    context 'when guessing a number that is lower than the secret number ' do
+      it 'returns the symbol :lower' do
+        expect(subject.guess(3)).to eq(:lower)
+      end
+    end
+    
+    context 'when guessing a number that is higher than the secret number ' do
+      it 'returns the symbol :greater' do
+        expect(subject.guess(7)).to eq(:greater)
       end
     end
   end
